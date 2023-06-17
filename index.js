324f4f7f845d62858b139f8f7d12132d40ed5981
Shamim <shamim@outlook.jp>
2023-06-17 23:35:06 +0600

    removed unused files

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ const verifyJWT = (req, res, next) => {
 }
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qtselqx.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
